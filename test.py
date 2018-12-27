6eb07dfbed592da549125cafb652a498e9671aa2
DapperDog <zacharukalexander@gmail.com>
2018-12-27 01:31:56 -0400

    First phase of checklists

--- test.py
+++ test.py
@@ -1,21 +1,26 @@
 import json
 import os
 
-for filename in os.listdir('cards'):
-    with open('cards/'+filename) as f:
-        print filename
-        data = json.load(f)
-        children = []
-        for v in data:
-            name = v["name"]
-            number = v["number"]
-            if "rarity" in v:
-                rarity = v["rarity"]
-            else:
-                rarity = "unknown"
-            series = v["series"]
-            set = v["set"]
-            id = v["id"]
-            children.append({"name": name,"number":number,"rarity":rarity,"series":series,"set":set,"id":id})
-        with open('simplifiedcards/'+filename, 'w') as outfile:
-            json.dump(children, outfile, indent=2)
+seriesName = 'Series/XY Series/'
+for filename in os.listdir(seriesName):
+    if filename!='simplifiedcards':
+        with open(seriesName+filename) as f:
+            print filename
+            data = json.load(f)
+            output = {"cardVariants":["core","reverse"],"version":1.0}
+            output['coreCards'] = []
+            for v in data:
+                name = v["name"]
+                number = v["number"]
+                if "rarity" in v:
+                    rarity = v["rarity"]
+                else:
+                    rarity = "unknown"
+                series = v["series"]
+                set = v["set"]
+                id = v["id"]
+                output['coreCards'].append({"name": name,"number":number,"rarity":rarity,"id":id})
+            with open(seriesName+'simplifiedcards/'+filename, 'w') as outfile:
+                output['additionalCards'] = []
+                output['other'] = []
+                json.dump(output, outfile, indent=2)
